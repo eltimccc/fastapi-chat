1d5b6bdcf912d45aa9e4f6aa9f1205738ac5e53f
eltimccc <eltimccc@yandex.ru>
2024-12-07 16:46:23 +0300

    structure

--- app/database.py
+++ app/database.py
@@ -1,11 +1,15 @@
 from sqlalchemy import func
 from datetime import datetime
 from sqlalchemy.orm import Mapped, mapped_column, DeclarativeBase
-from sqlalchemy.ext.asyncio import AsyncAttrs, async_sessionmaker, create_async_engine, AsyncSession
+from sqlalchemy.ext.asyncio import (AsyncAttrs,
+                                    async_sessionmaker,
+                                    create_async_engine,
+                                    AsyncSession)
 
-database_url = 'sqlite+aiosqlite:///db.sqlite3'
+
+database_url = 'sqlite+aoisqlite:///db.sqlite3'
 engine = create_async_engine(url=database_url)
-async_session_maker = async_sessionmaker(engine, class_=AsyncSession)
+async_sessionmaker = async_sessionmaker(engine, class_= AsyncSession)
 
 
 class Base(AsyncAttrs, DeclarativeBase):
---
+++ app/users/models.py
@@ -0,0 +1,3 @@
+from sqlalchemy import String, Integer
+from sqlalchemy.orm import Mapped, mapped_column
+from database import Base
